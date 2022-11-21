2f832e60b01410d80820d7a77b7f20aec9c147fe
David C Black <david.black@doulos.com>
2022-11-21 12:54:05 -0600

    Note location of SystemC to help with debug
    Signed-off-by: David C Black <david.black@doulos.com>

--- cmake/systemc.cmake
+++ cmake/systemc.cmake
@@ -2,6 +2,7 @@
 
 # SystemC specific needs
 find_package( SystemCLanguage CONFIG REQUIRED PATHS "$ENV{SYSTEMC_HOME}" )
+message( "SYSTEMC_HOME=$ENV{SYSTEMC_HOME}" )
 
 set (CMAKE_CXX_STANDARD ${SystemC_CXX_STANDARD} CACHE STRING "C++ standard to build all targets. Supported values are 98, 11, 14, and 17.")
 set (CMAKE_CXX_STANDARD_REQUIRED ${SystemC_CXX_STANDARD_REQUIRED} CACHE BOOL "The CMAKE_CXX_STANDARD selected C++ standard is a requirement.")
